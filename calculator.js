2114069fa125754c0b5d25e97dfc8e9b85145fda
Aman Joshi <amanjoshi149@gmail.com>
2023-07-01 12:15:48 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 // the home route
 app.get("/",function (request,response) {
-    // response.send("<h1>Hello World!</h1>");
-
     response.sendFile(__dirname + "/index.html");
 });
 
 // the BMI calculator route
 app.get("/bmicalculator",function (request,response) {
-    // response.send("<h1>Hello World!</h1>");
     response.sendFile(__dirname + "/bmiCalculator.html");
 });
 
 app.post("/bmicalculator",function (request,response) {
-    // response.send("<h1>Hello World!</h1>");
     var weight = parseFloat(request.body.weight);
     var height = parseFloat(request.body.height);
     var bmi = weight / (height * height);
@@ -30,7 +24,6 @@ app.post("/bmicalculator",function (request,response) {
 
 app.post("/",function (request,response) { 
     console.log(request.body);
-    // response.send("Thanks for posting that!");
     var num1 = Number(request.body.num1);
     var num2 = Number(request.body.num2);
     var result = num1 + num2;
